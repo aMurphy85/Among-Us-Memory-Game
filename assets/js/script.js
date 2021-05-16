271b5e6787c0f19d41882bc54de1d99699bf3d04
aMurphy85 <murv121@hotmail.co.uk>
2021-05-16 19:34:14 +0000

    Create functions to check if cards are a match and if they don't flip back over after a setTimeOut

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
 const card = document.querySelectorAll(".gameCard")
 
+let flippedCard = false;
+let firstCard, secondCard;
+
+// Creating a flipcard function
 function flipCard() {
+
     this.classList.toggle('flip');
-    console.log("I was clicked");
-    console.log(this);
+
+    if(!flippedCard) {
+        flippedCard = true;
+        firstCard = this;
+    } else {
+        flippedCard = false;
+        secondCard = this;
+
+        cardMatch();
+    }    
 }
+
+function cardMatch() {
+    if(firstCard.dataset.image === secondCard.dataset.image) {
+            cardsMatch();
+        } else {
+            resetCards();
+        }
+}
+
+function cardsMatch() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function resetCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1200);
+}
+
+   // console.log("function worked!");
+
 
 card.forEach(card => card.addEventListener('click',flipCard));
